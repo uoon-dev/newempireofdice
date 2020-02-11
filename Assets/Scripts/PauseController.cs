eb24c5feb23265cbaf6b01b6d71879b338096b6b
fox <uoon.dev@gmail.com>
2020-02-11 17:55:09 +0900

    add stage text component in play scene

--- Assets/Scripts/PauseController.cs
+++ Assets/Scripts/PauseController.cs
@@ -2,14 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class PauseController : MonoBehaviour
 {
     [SerializeField] GameObject PauseCanvas = null;
+    LevelLoader levelLoader;
+    Text stageText;
 
     void Start()
     {
+        Initialize();
         HideScreen();
+    }
+
+    private void Initialize()
+    {
+        levelLoader = FindObjectOfType<LevelLoader>(); 
+
+        stageText = GameObject.Find(Constants.GAME_OBJECT_NAME.STAGE_TEXT).GetComponent<Text>();
+        stageText.text = $"Stage {levelLoader.GetCurrentLevelNumber()}";
     }
 
     public void ShowScreen()
@@ -20,11 +32,16 @@ public class PauseController : MonoBehaviour
         }
         PauseCanvas.GetComponent<Canvas>().sortingOrder = 13;
         PauseCanvas.SetActive(true);
-
+        // PauseCanvas.GetComponent<CanvasGroup>().alpha = 1;
+        // PauseCanvas.GetComponent<CanvasGroup>().interactable = true;
+        // PauseCanvas.GetComponent<CanvasGroup>().blocksRaycasts = true;
     }
 
     public void HideScreen()
     {
         PauseCanvas.SetActive(false);
+        // PauseCanvas.GetComponent<CanvasGroup>().alpha = 0;
+        // PauseCanvas.GetComponent<CanvasGroup>().interactable = true;
+        // PauseCanvas.GetComponent<CanvasGroup>().blocksRaycasts = true;        
     }
 }
