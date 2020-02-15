2becfcbdcebb4f397a290851322a90a8cd71aa5b
fox <uoon.dev@gmail.com>
2020-02-15 16:43:23 +0900

    update guide canvas policy

--- Assets/Scripts/GuideCanvasController.cs
+++ Assets/Scripts/GuideCanvasController.cs
@@ -9,13 +9,22 @@ public class GuideCanvasController : MonoBehaviour
     // Start is called before the first frame update
     public static string guideType = null;
 
+    // void Start()
+    // {
+    //     ToggleGuideCanvas(true);
+    // }
+
     public void ToggleGuideCanvas(bool isShow)
     {
         this.gameObject.SetActive(isShow);
-        var body = this.gameObject.transform.GetChild(0);
+        var body = this.gameObject.transform.Find(Constants.GAME_OBJECT_NAME.BODY);
+        var panelSetting = this.gameObject.transform.Find(Constants.GAME_OBJECT_NAME.PANEL_SETTING);
 
-        if (isShow) {
-            body.transform.DOMoveY(Screen.height/2, 0.25f).SetDelay(1f);
+        if (isShow)
+        {
+            body.transform.DOMoveY(Screen.height/2, 0.25f).SetDelay(1f).OnComplete(() => {
+                panelSetting.gameObject.SetActive(true);
+            });
             return;
         }
         body.transform.DOMoveY(-Screen.height/2, 0.25f).SetDelay(1f);
